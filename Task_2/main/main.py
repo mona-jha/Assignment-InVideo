18cb4a8801832fcc5b371d6c16d3d2ae191383e8
Mona <71166547+mona-jha@users.noreply.github.com>
2025-04-24 06:56:26 +0530

    folder name

--- Task_2/main/main.py
+++ Task_2/main/main.py
@@ -7,10 +7,10 @@ import argparse
 import torch
 from tqdm import tqdm
 
-from .main.models import DinoEmbedder, EmbeddingToImageGenerator
-from .datasets import FaceDataset, EmbeddingImageDataset, create_dataloaders
-from .training import train_embedder, train_gan
-from .evaluation import generate_images, test_zero_shot_generalization
+from main.models import DinoEmbedder, EmbeddingToImageGenerator
+from utils.datasets import FaceDataset, EmbeddingImageDataset, create_dataloaders
+from utils.training import train_embedder, train_gan
+from utils.evaluation import generate_images, test_zero_shot_generalization
 from .utils import setup_device
 
 def parse_args():
